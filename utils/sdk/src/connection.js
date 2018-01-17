bb4dc08ae3ac5d6d754ffc8f465121a9747a1be2
owldamon <damonowl@gmail.com>
2018-01-17 16:30:15 +0800

    合并

--- utils/sdk/src/connection.js
+++ utils/sdk/src/connection.js
@@ -831,11 +831,7 @@ connection.prototype.open = function (options) {
             success: suc || _utils.emptyfn,
             error: error || _utils.emptyfn
         };
-<<<<<<< HEAD
         // console.log("options", options.success)
-=======
-        console.log("options", options.success)
->>>>>>> 1f16ec1df534b5f96ae4624d7cdef4eaedd6dcc5
         _utils.ajax(options);
     }
 
